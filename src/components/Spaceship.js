d505d7417c8a0ada21056b71c7d7cb06a5d9d463
noven temer <noven5823@gmail.com>
2018-09-24 13:21:51 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,14 @@
 // Code The Spaceship Component Here
+import React from 'react'
+
+ class Spaceship extends React.Component(){
+    render(){
+        const {name, speed, hasRockets, colors} = this.props
+    }
+      
+  
+}
+
+
+
+export default Spaceship;
